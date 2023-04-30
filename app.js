556ca37c8518dbed43b70ca36be2a4caba8bcb15
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-30 09:22:58 -0300

    added all from concacaf test

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import { rankingFifa as teams, teamsByConfed } from "./fwcSpecifications.js";
 
 function concacafClasification() {
-  let concacafQualified = Array.from(
-    { length: teamsByConfed.CONCACAF },
-    () => []
-  );
-  return concacafQualified;
+  let concacafQualified = [];
+  teams.map((t) => {
+    if (
+      t.confederation === "CONCACAF" &&
+      concacafQualified.length < teamsByConfed.CONCACAF
+    ) {
+      concacafQualified.push(t);
+    } else {
+      return;
+    }
+  });
+  return concacafQualified
 }
 
 concacafClasification();
--- app.test.js
+++ app.test.js
@@ -1,7 +1,25 @@
 import concacafClasification from "./app";
 import { teamsByConfed } from "./fwcSpecifications";
 
-test("return the qualified teams by hosts", () => {
+describe("CONCACAF classification", () => {
+
+  // beforeAll()
+
+  test("return the correct quantity of concacaf qualified teams", () => {
+    const concacafAllQualified = concacafClasification()
+    expect(concacafAllQualified.length).toBe(teamsByConfed.CONCACAF);
+  });
   
-  expect(concacafClasification().length).toBe(teamsByConfed.CONCACAF);
-});
+  test("check that all qualified are from concacaf", () => {
+    const concacafAllQualified = concacafClasification()
+    const returnedQualifiedConfeds = []
+    concacafAllQualified.map(t => {
+      returnedQualifiedConfeds.push(t.confederation)
+    })
+    const areAllFromConcacaf = returnedQualifiedConfeds.every(c => c === "CONCACAF")
+    expect(areAllFromConcacaf).toBe(true)
+  })
+
+  test.todo("returned array should contains the hosts teams")
+})
+
