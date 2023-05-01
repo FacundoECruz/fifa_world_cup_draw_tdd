191ca2e5b1cdaceb28df34d6f134223833cf5b08
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-05-01 09:33:21 -0300

    concacaf classif to separate file

---
+++ concacafClassification.js
@@ -0,0 +1,27 @@
+import { rankingFifa as allTeams, teamsByConfed } from "./fwcSpecifications.js";
+
+function concacafClasification() {
+  const hostsRequired = ["México", "Estados Unidos", "Canada"];
+  const concacafTeams = allTeams.filter(team => team.confederation === "CONCACAF")
+  let hosts = []
+  let noHosts = []
+
+  concacafTeams.map(team => {
+    if(team.name === hostsRequired[0] ||
+      team.name === hostsRequired[1] ||
+      team.name === hostsRequired[2]) {
+        hosts.push(team)
+      } else {
+        noHosts.push(team)
+      }
+  })
+  
+  const noHostsRequired = teamsByConfed.CONCACAF - hosts.length;
+  const noHostsQualified = noHosts.slice(0, noHostsRequired);
+  const concacafQualified = hosts.concat(noHostsQualified)
+
+  console.log(concacafQualified);
+  return concacafQualified;
+}
+
+export default concacafClasification;
